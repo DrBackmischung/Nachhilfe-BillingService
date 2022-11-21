e55468bc30523d6eeafb564caf9298001179e930
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-11-21 22:40:31 +0100

    work with data

--- nachhilfe/google/views.py
+++ nachhilfe/google/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 
 def index(request):
-    return HttpResponse("Hello, world. Google here!")
+    json_data = json.loads(request.body)
+    article = json_data['article']
+    price = json_data['price']
+    street = json_data['street']
+    houseNr = json_data['houseNr']
+    zipCode = json_data['zipCode']
+    city = json_data['city']
+    return HttpResponse(json_data['street'])
