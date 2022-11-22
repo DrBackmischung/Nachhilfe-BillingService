c8d28288a0d8515a77ca8c5434b796b588f03e70
DrBackmischung aka Mathis <mathis.neunzig@gmail.com>
2022-11-22 20:27:27 +0100

    change url

--- nachhilfe/nachhilfe/urls.py
+++ nachhilfe/nachhilfe/urls.py
@@ -19,5 +19,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('google/', include('google.urls')),
+    path('billing/', include('google.urls')),
 ]
